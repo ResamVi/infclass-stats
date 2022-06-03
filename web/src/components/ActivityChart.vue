0e124c8da5d9fd71a4fd0871ac2d5e1110981bbc
Julien Midedji <midedji@jacob.de>
2022-06-03 22:14:44 +0200

    all good

--- web/src/components/ActivityChart.vue
+++ web/src/components/ActivityChart.vue
@@ -50,9 +50,6 @@ export default {
         xaxis: {
           labels: {
             formatter(value, timestamp) {
-              console.log(value);
-              console.log(timestamp);
-              console.log("---");
               const date = new Date(timestamp * 1000);
               const day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][date.getDay()];
               const hours = `0${date.getHours()}`;
