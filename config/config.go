9d97ea3f572a37d32571ed72753e4955ef17c36c
Julien Midedji <midedji@jacob.de>
2022-06-11 13:31:31 +0200

    init config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,18 @@ package config
 
 import "os"
 
+func init() {
+	SERVER_IP = os.Getenv("SERVER_IP")
+	ECON_PORT = os.Getenv("ECON_PORT")
+	ECON_PASSWORD = os.Getenv("ECON_PASSWORD")
+	DEBUG_MODE = true
+	SERVE_API = true
+
+	MYSQL_USER = os.Getenv("MYSQL_USER")
+	MYSQL_PASSWORD = os.Getenv("MYSQL_PASSWORD")
+	MYSQL_DB = os.Getenv("MYSQL_DB")
+}
+
 var (
 	// SERVER_IP is the IP to the machine running the teeworlds server
 	SERVER_IP = os.Getenv("SERVER_IP")
